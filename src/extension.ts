de7aa5f93f4f7e8bd29df1786b1e5e5746c600fd
Sunil Shrestha <shrest464760+github@gmail.com>
2023-04-26 19:54:22 +0545

    Added a new command

--- src/extension.ts
+++ src/extension.ts
@@ -13,13 +13,19 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('tailwind-to-css-generator.helloWorld', () => {
+	let disposable = vscode.commands.registerCommand('tailwind-to-css-generator.helloVSCode', (a) => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
+		console.log(a);
 		vscode.window.showInformationMessage('Hello World from tailwind-to-css-generator!');
+	});
+	let disposable2 = vscode.commands.registerCommand('tailwind-to-css-generator.convertTailwindToCSS', (a) => {
+		console.log(a);
+		vscode.window.showInformationMessage('Hello again from tailwind-to-css-generator!');
 	});
 
 	context.subscriptions.push(disposable);
+	context.subscriptions.push(disposable2);
 }
 
 // This method is called when your extension is deactivated
